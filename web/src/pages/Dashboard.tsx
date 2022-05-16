3751c7dae147705410c176f0b1978db632d2ed0d
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-16 13:21:18 +0300

    add(DashBoard): LineGraphs

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import LineGraphs from '.././components/LineGraphs'
 import { Container } from 'react-bootstrap'
 import NavBar from '.././components/NavBar'
+import DaysCard from '../components/DaysCard'
 
 interface IPros {
   user: string | null
   handleLogout: () => void
+  addData: (days: number, amount: number) => void
+  data: number[]
 }
 
-const DashBoard: React.FC<IPros> = ({ user, handleLogout }) => {
+const DashBoard: React.FC<IPros> = ({ user, handleLogout, addData, data }) => {
   return (
     <div>
       <NavBar user={user} handleLogout={handleLogout} />
       <Container className='text-center' >
-        <LineGraphs />
+        <LineGraphs data={data} />
       </Container >
+      <DaysCard addData={addData} />
     </div >
   )
 }
