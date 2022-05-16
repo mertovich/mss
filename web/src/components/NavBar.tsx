1aef4e91198101447c8860bb1be3bdd5e28d6928
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-16 13:30:02 +0300

    add(NavBar): Clear DashBoard

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -4,9 +4,10 @@ import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 interface IPros {
     user: string | null
     handleLogout: () => void
+    clearDashBoard: () => void
 }
 
-const NavBar: React.FC<IPros> = ({ user,handleLogout }) => {
+const NavBar: React.FC<IPros> = ({ user,handleLogout, clearDashBoard }) => {
     return (
         <div>
             <Navbar bg="light" variant="light">
@@ -15,6 +16,7 @@ const NavBar: React.FC<IPros> = ({ user,handleLogout }) => {
                     }} >
                     <Nav>
                         <NavDropdown title={`Welcome ${user}`} id="navbarScrollingDropdown">
+                            <NavDropdown.Item onClick={clearDashBoard} href="#action3">Clear DashBoard</NavDropdown.Item>
                             <NavDropdown.Item onClick={handleLogout} href="#action3">Log out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
