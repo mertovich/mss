644301531e13ddb99419d1f7f63495ad093240d7
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-16 06:49:58 +0300

    add(LoginControl)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import DashBoard from './pages/DashBoard'
+import Login from './pages/Login'
+
 
 type Props = {}
 
 const App = (props: Props) => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+  const [user, setUser] = useState<string | null>(null)
+
+  const handleLogin = (user: string) => {
+    setIsLoggedIn(true)
+    setUser(user)
+    console.log(user)
+    localStorage.setItem('user', user)
+    localStorage.setItem('isLoggedIn', 'true')
+  }
+
+  const loginControl = () => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn')
+    if (isLoggedIn === 'true') {
+      setIsLoggedIn(true)
+      setUser(localStorage.getItem('user'))
+    }
+  }
+
+  useEffect(() => {
+    loginControl()
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        {isLoggedIn ?
+          <Route path="/" element={<DashBoard user={user} />} />
+          :
+          <Route path="/" element={<Login handleLogin={handleLogin} />} />
+        }
+      </Routes>
+    </div>
   )
 }
 
