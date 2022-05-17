77af3e5df3dbae65c04c2eb60d35c6a19ec04dba
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-17 10:42:27 +0300

    add(Login) button color

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -25,7 +25,7 @@ const Login: React.FC<IProps> = ({ handleLogin }) => {
                         <Form.Group className="mb-3" controlId="name">
                             <Form.Control onChange={(e) => setUser(e.target.value)} type="name" placeholder="Enter name" />
                         </Form.Group>
-                        <Button onClick={() => handleLogin(user)} variant="primary" type="submit">
+                        <Button onClick={() => handleLogin(user)} variant="primary" type="submit" style={{backgroundColor: '#FFD24C', borderColor: '#FFD24C'}} >
                             Submit
                         </Button>
                     </Form>
